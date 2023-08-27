9dfcef1dc0c2c8c67a3e176bfaf782e464ac98bd
dragon <1573511441@qq.com>
2023-08-27 22:22:30 +0800

    feat: 添加 全局的一级路由

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,6 +3,10 @@ module.exports = (app) => {
   fs.readdirSync(__dirname).forEach(file => {
     if (file === 'index.js') { return; }
     const route = require(`./${file}`);
+
+    // 全局的一级路由
+    route.prefix('/api');
+
     app.use(route.routes()).use(route.allowedMethods());
   });
 }
