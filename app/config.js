5808a43089fb2ec98faf8f775afe188fd4654179
dragon <1573511441@qq.com>
2023-08-27 17:59:12 +0800

    feat: init readme

---
+++ app/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  secret: 'zhihu-jwt-secret',
+  // connectionStr: 'mongodb+srv://lewis:mukewang@zhihu-kag3y.mongodb.net/test?retryWrites=true',
+  connectionStr: 'mongodb://127.0.0.1:27017/imooc-zhihu-api', // 数据库名字
+};
